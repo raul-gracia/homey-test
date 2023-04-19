78fa69cd8723f7f56fb5e42e3818a573dc704a67
Raul Gracia <raulgracialario@gmail.com>
2023-04-19 01:52:29 +0100

    adding devise with tailwind views

--- homey/config/routes.rb
+++ homey/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'game/index'
+  root to: 'game#index'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
